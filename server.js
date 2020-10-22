5e990ead4386d3fca8bc7c63016eb8940b2128bd
Matthew Day <matt@matthewday.net>
2020-10-22 17:06:55 -0400

    Remove unnecessary dependency from step 02-01

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(bodyParser.json());
 
 app.get('/heartbeat', (req, res) => {
   res.json({
