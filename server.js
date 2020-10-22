765b49c3db66e5726193bdf38a175a093f827c38
Matthew Day <matt@matthewday.net>
2020-10-22 17:03:15 -0400

    Add step 03-01

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/css', express.static(__dirname + "/css"));
+app.use('/js', express.static(__dirname + "/js"));
+app.use('/', express.static(__dirname + "/public"));
+
 app.get('/heartbeat', (req, res) => {
   res.json({
     is: "working"
