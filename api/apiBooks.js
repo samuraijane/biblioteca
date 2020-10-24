f8500a8ed84fc32506d9d464c559d7354fb72d38
Matthew Day <matt@matthewday.net>
2020-10-24 00:39:21 -0400

    Add step 04-01

---
+++ api/apiBooks.js
@@ -0,0 +1,13 @@
+module.exports = (app, fetch) => {
+
+  // -----------------------------------------------------------------------------
+  //                                     GET
+  // -----------------------------------------------------------------------------
+  app.get("/api/books", async (req, res) => {
+    const { author, title } = req.body;
+    const params = `${title ? "title=" + title : ''}${author ? "&author=" + author : ''}`;
+    fetch(`http://openlibrary.org/search.json?${params}&limit=10`)
+      .then(result => result.json())
+      .then(data => res.json(data));
+  });
+};
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const fetch = require('node-fetch');
+
+const apiBooks = require('./api/apiBooks');
+const apiHeartbeat = require('./api/apiHeartbeat');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use('/css', express.static(__dirname + "/css"));
 app.use('/js', express.static(__dirname + "/js"));
 app.use('/', express.static(__dirname + "/public"));
 
-app.get('/heartbeat', (req, res) => {
-  res.json({
-    is: "working"
-  })
-});
+apiBooks(app, fetch);
+apiHeartbeat(app);
 
 app.listen(3001, () => {
   console.log('The server is running at port 3001');
