f858898e7607ae26ffa6d673e360858f1c13c2b4
Matthew Day <matt@matthewday.net>
2020-11-05 16:58:37 -0500

    Account for dynamic partials for landing and dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,6 +4,6 @@ module.exports = (app, ensureAuthenticated) => {
   //                                     GET
   // -----------------------------------------------------------------------------
   app.get("/dashboard", ensureAuthenticated, async (req, res) => {
-    res.render('pages/dashboard');
+    res.render('pages', {template: 'dashboard'});
   });
 };
